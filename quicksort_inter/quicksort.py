a6104937ef2218c3f491aa356f79adc225694388
samigamer1999 <55395718+samigamer1999@users.noreply.github.com>
2020-06-26 10:36:23 +0200

    Update quicksort.py

--- quicksort_inter/quicksort.py
+++ quicksort_inter/quicksort.py
@@ -1,5 +1,6 @@
 from ctypes import *
 from numpy.ctypeslib import ndpointer
+import time
 
 # On charge le module go compilé
 lib = cdll.LoadLibrary("./quicksort.so")
@@ -14,6 +15,10 @@ lib.Sort.restype = ndpointer(dtype = c_double, shape = (len(pyarr),))
 # Crée un C array à la base d'un py array 
 arr = (c_double * len(pyarr))(*pyarr)
 
-# Print le résultat
+t1 = time.clock()
+# On calcule le résultat
 result = lib.Sort(arr, len(arr))
+t2 = time.clock() - t1
+print("Temps d'éxection :", t2)
+
 print(result)
