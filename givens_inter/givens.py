01cc23802d380ab1bf93eb7629277efbbcb5d87d
samigamer1999 <55395718+samigamer1999@users.noreply.github.com>
2020-06-26 10:32:35 +0200

    Update givens.py

--- givens_inter/givens.py
+++ givens_inter/givens.py
@@ -1,6 +1,6 @@
 from ctypes import *
 from numpy.ctypeslib import ndpointer
-
+import time
 # On charge le module go compilé
 lib = cdll.LoadLibrary("./givens.so")
 
@@ -19,9 +19,12 @@ lib.QR.restype = ndpointer(dtype = c_double, shape = (2*len(pyarr),))
 
 # Crée un C array à la base d'un py array 
 arr = (c_double * len(pyarr))(*pyarr)
+
+t1 = time.clock()
 # qr_array est une liste à 1D qui contient R et Q
 qr_array = lib.QR(arr, len(arr), rows, cols)
-
+t2 = time.clock() - t1
+print(t2) # Temps d'éxectution
 # On reconstruit les deux matrices grâce à qr_array
 r = qr_array[0:rows * cols].reshape(rows, cols)
 q = qr_array[rows * cols:].reshape(rows, cols)
